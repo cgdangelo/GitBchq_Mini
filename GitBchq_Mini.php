4c5efaa40036126ec0dda520fa37283c1d63b315
Charles D'Angelo <c.g.dangelo@gmail.com>
2012-01-03 00:00:10 -0500

    Added more request stuff mostly
    * Added POST request method
    
    * Added ability to upload patch files

--- GitBchq_Mini.php
+++ GitBchq_Mini.php
@@ -7,6 +7,7 @@ class GitBchq_Mini {
     protected $_apiKey;
     protected $_baseUrl;
     protected $_projectId;
+    private $_curl;
 
     public function __construct($apiKey, $baseUrl, $projectId) {
         $this->_apiKey = $apiKey;
@@ -17,26 +18,57 @@ class GitBchq_Mini {
     public function buildRoute(Array $urlArgs) {
         $request = '';
         foreach($urlArgs as $arg => $value) {
-            $request .= "/$arg/$value";
+            $request .= "$arg/$value/";
         }
 
-        return $request;
+        return rtrim($request, '/');
     }
 
-    private function curl($route = '') {
-        $curl = curl_init($this->_baseUrl . $route);
-        curl_setopt_array($curl, array(
-            CURLOPT_HTTPHEADER => array(
-                'Accept: application/xml',
-                'Content-type: application/xml'
-            ),
-            
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_USERPWD => $this->_apiKey . ":"
-        ));
+    private function curl($route = null) {
+        if(!isset($this->_curl) || get_resource_type($this->_curl) != 'curl') {
+            $this->_curl = curl_init();
 
-        return curl_exec($curl);
+            curl_setopt_array($this->_curl, array(
+                CURLOPT_HTTPHEADER => array(
+                    'Accept: application/xml',
+                    'Content-type: application/xml'
+                ),
+                
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_USERPWD => $this->_apiKey . ":",
+                CURLOPT_VERBOSE => true
+            ));
+        }
+
+        if(is_array($route)) {
+            $route = $this->buildRoute($route);
+        }
+
+        curl_setopt($this->_curl, CURLOPT_URL, $this->_baseUrl . $route);
+
+        return $this->_curl;
+    }
+
+    public function get($route = null) {
+        return curl_exec($this->curl($route));
+    }
+
+    public function post($route = null, $formData) {
+        curl_setopt_array($this->curl(), array(
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $formData
+        ));
+        
+        return curl_exec($this->curl($route));
+    }
+
+    private function put() {
+
+    }
+
+    private function delete() {
+
     }
 
     public function getMessages() {
@@ -45,7 +77,7 @@ class GitBchq_Mini {
             'posts' => ''
         ));
 
-        $messagesXml = new SimpleXMLElement($this->curl($route));
+        $messagesXml = new SimpleXMLElement($this->get($route));
         $messagesList = array();
         foreach($messagesXml as $message) {
             $messagesList["$message->id"] = "$message->title";
@@ -74,10 +106,26 @@ class GitBchq_Mini {
 
         return $diff;
     }
+
+    public function uploadPatch() {
+        $patchDiff = $this->getPatch();
+
+        curl_setopt_array($this->curl(), array(
+            CURLOPT_HTTPHEADER => array(
+                'Content-type: application/octet-stream',
+                'Content-length: ' . strlen($patchDiff)
+            ),
+            CURLOPT_VERBOSE => true
+        ));
+
+        $response = $this->post('upload', $patchDiff);
+        curl_close($this->curl());
+        
+        return $response;
+    }
 }
 
 $BCHQ_APIKEY=trim(`git config --get basecamp.apikey`);
 $BCHQ_BASE_URL=trim(`git config --get basecamp.baseurl`);
 $BCHQ_PROJECT_ID=trim(`git config --get basecamp.projectid`);
 $GitBchq = new GitBchq_Mini($BCHQ_APIKEY, $BCHQ_BASE_URL, $BCHQ_PROJECT_ID);
-var_dump($GitBchq->getLastCommit());
